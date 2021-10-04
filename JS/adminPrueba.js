686ca8ead6158d23c2a730a54098480722ad6a8d
JhoanGarcia95 <jhoangarcia95.jg@gmail.com>
2021-10-04 11:42:56 -0500

    Avance JS página de admin

---
+++ JS/adminPrueba.js
@@ -0,0 +1,99 @@
+var currentTab=0;
+var x=document.getElementsByClassName("tag");
+
+//Usuario temporal
+const user=["usuario21","admin1234"];
+
+//Función que muestra las pestañas del formulario
+function mostrar(n){
+    x[n].style.display="block";
+    if(n<=1){
+        document.getElementById("btnAnt").style.display="none";
+    }else{
+        document.getElementById("btnAnt").style.display="inline";
+    }
+    if(n==(x.length-1)){
+        document.getElementById("btnSig").innerHTML="Guardar";
+        document.getElementById("vistaPrev").style.display="inline";
+    }else{
+        if (n==0){
+            document.getElementById("btnSig").innerHTML="Ingresar";
+        }else{
+            document.getElementById("btnSig").innerHTML="Continuar";
+        }
+        document.getElementById("vistaPrev").style.display="none";
+    }
+    indicStep(n);
+}
+
+//Validación ingreso de datos en cajas de texto y usuario y contraseña
+function validacion(){
+    var y, i, valido=true;
+    y=x[currentTab].getElementsByTagName("input");
+    for (i=0; i<y.length;i++){
+        if (y[i].value==""){
+            y[i].className+=" invalido";
+            valido=false;
+        }
+    }
+    switch(currentTab){
+        case 1:
+            const radio=document.getElementsByName("actualizacion_menu");
+            valido=false;
+            for (i=0;i<radio.length;i++){
+                if(radio[i].checked){
+                    valido=true;
+                }
+            }
+            if(!valido){document.getElementById("mens_error").style.display="block"}
+    }
+    return valido;
+}
+
+//Función a ejecutarse en los botones de atrás / siguiente
+function btnPrev(n){
+    if (n==1 && !validacion()) {return false} //Evaluación validación cuando se oprime el botón siguiente
+    x[currentTab].style.display="none"; //Oculta pestaña actual
+    currentTab=currentTab+n;
+    if (currentTab>=x.length){
+        //Función a ejecutar cuando se oprime el botón de siguiente en la última pestaña
+        //Función provisional mientras base de datos ON
+        const mens=document.createElement("P");
+        const text=document.createTextNode("Datos del plato guardados con éxito");
+        mens.appendChild(text);
+        const prev=document.getElementsByClassName("button")[0];
+        const parent=document.getElementById("modificacion_menu");
+        parent.insertBefore(mens,prev);
+        prev.style.display="none"; //Oculta los botones al final
+        indicStep(currentTab);
+        return false;
+    }
+    mostrar(currentTab);
+}
+
+//Función que indica la pestaña del formulario en la que se encuentra actualmente
+function indicStep(n){
+    var y=document.getElementsByClassName("step");
+    var i;
+    //Aplicación estilo base a todos los elementos de indicación de página
+    for (i=0;i<y.length;i++){
+        y[i].style.opacity=0.5;
+    }
+    if (n>=y.length){
+        return false; //Salir de la función al pasar de la última pestaña del formulario
+    }
+    y[n].style.opacity=0.8; //Cambio de opacidad para indicar la pestaña actual
+}
+
+//Función de creación automática del número de indicadores que se requieran según el número de pestañas del formulario
+function stepCreation(){
+    var i;
+    var cont=document.getElementById("indicador"); //Contenedor de los indicadores
+    for (i=0;i<x.length;i++){
+        var ind=document.createElement("SPAN"); //Creación nodo tipo span
+        var att=document.createAttribute("class"); //Creación de nodo atributo tipo class
+        att.value="step";
+        ind.setAttributeNode(att); //Adición nodo atributo al nodo elemento
+        cont.appendChild(ind); //Adición nodo span al div indicador
+    }
+}
