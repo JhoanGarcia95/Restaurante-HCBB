51c0a2e1eb8fa9785d7ba107f89248d3a33c835d
Diego Fernando Meneses Ballesteros <balmung.programming@gmail.com>
2021-10-15 13:07:34 -0500

    Inclusion de proyecto en Vue para el Modulo Menu

---
+++ restaurante_hcbb_web/src/components/Carta.vue
@@ -0,0 +1,318 @@
+<template>
+    <div id="Carta">
+        <h2>La Carta</h2>
+        <h4><em>¡Estas son nuestras mejores recetas!</em> </h4>
+            <div id="carrusel" class="carrusel_cajaExterna" data-bs-ride="carousel">
+                <div class="carrusel_cajaInterna">
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/arroz-Atollado.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Arroz Atollado</b></h2>
+                                <hr>
+                                <p>Recibió este nombre porque en sus inicios se preparó con Tollo,<br> una especie perteneciente 
+                                a la familia de los tiburones.<br>Aunque algunas personas lo siguen<br> 
+                                cocinando con pescado, la mayoría de platos se preparan con carne de cerdo y pollo hoy en día.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong>arroz, pechuga de pollo grande, chuletas de carne de cerdo, chorizos, 
+                                costillas de cerdo, papas, azafrán o pimentón dulce.<br>
+                                </p> 
+                            </div>       
+                        </div> 
+                    </div>                            
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/arroz-Camarones.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Arroz con Camarones</b></h2>
+                                <hr>
+                                <p>Este arroz con camarones puede disfrutarse en cualquier época del año.<br>
+                                Le llamo a este plato arroz con camarones criollos, porque los mariscos<br>
+                                se saltean en nuestra salsa más típica, un sofrito que llamamos hogao.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> arroz, caldo de pescado, cúrcuma, achiote o color, centrado de camarón, 
+                                alverjas, zanahoria, pimentón rojo, camarones.<br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/arroz-Carnes.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Arroz con Carnes</b></h2>
+                                <hr>
+                                <p>Al arroz se le pueden agregar infinidad de acompañantes
+                                y crear combinaciones maravillosas. <br>Hoy decidimos combinar arroz con pollo y chorizo.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> chorizos, arroz, pollo asado, zanahoria, alverjas, granos de maíz.<br>
+                                </p> 
+                            </div>
+                        </div>     
+                    </div>            
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/arroz-Paisa.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Arroz Paisa</b></h2>
+                                <hr>
+                                <p>Mezclar arroz cocido con las carnes ya hechas: chorizo, tocineta y chicharrón.<br>
+                                El siguiente paso es agregar una cantidad al gusto de hogao,<br> cubitos de plátano maduro frito 
+                                y granos de maíz cocido.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> arroz, tocino, chorizos, tocineta, plátano maduro en cubos, maíz desgranado.<br>
+                                </p>
+                            </div>
+                        </div>          
+                    </div>  
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/arroz-Pollo.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Arroz con Pollo</b></h2>
+                                <hr>
+                                <p>Se frien un poco los trozos de pollo, que han sido adobados<br> con ajo previamente. 
+                                Luego añado las verduras que se dejan sofreír por un<br> 
+                                tiempo corto, para terminar la receta incorporando el arroz.<br></p> 
+                                <p><strong>Ingredientes:</strong> pechugas de pollo, zanahoria, arvejas, habichuelas, arroz, cúrcuma.<br>
+                                </p> 
+                            </div>
+                        </div>          
+                    </div> 
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/bagre-Salsa.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Bagre en Salsa</b></h2>
+                                <hr>
+                                <p>Otras opciones comunes de acompañamiento son unas rodajas de aguacate, trozos de yuca y plátano sancochados.
+                                Aunque para mí la combinación ideal es la del pescado con el arroz, el cual se mezcla a la perfección con la 
+                                deliciosa salsa.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> rodajas de bagre, leche de coco.<br>
+                                </p>  
+                            </div>
+                        </div>          
+                    </div> 
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/chuleta-Valluna.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Chuleta Valluna</b></h2>
+                                <hr>
+                                <p>Se prepara ablandando la carne con un rodillo o un mazo, para luego sazonarla y apanarla.<br>
+                                Normalmente se sirve con arroz blanco, papitas fritas y una ensalada,<br>
+                                y algunos prefieren agregarle un poco de limón.<br></p> 
+                                <p><strong>Ingredientes:</strong> chuleta de cerdo, harina de trigo, huevo, miga de pan<br>
+                                </p> 
+                            </div> 
+                        </div>          
+                    </div> 
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/corrientazo-Tipico.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Corrientazo Típico</b></h2>
+                                <hr>
+                                <p>Las lentejas con chorizo son de esos platos consistentes<br> acompañadas de plátano maduro frito, un 
+                                toque dulce que en mi opinión personal.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> lentejas, chorizos en rodajas, tocineta, zanahoria, papas, laurel.<br>
+                                </p>  
+                            </div>
+                        </div>          
+                    </div>
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/poteca-Ahuyama.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Poteca de Ahuyama</b></h2>
+                                <hr>
+                                <p>Ella suele servirla con arroz blanco y alguna carne guisada,<br> para sacarle el máximo provecho mezclando todo 
+                                lo que hay en el plato y convertirlo en una impresionante combinación de sabores y texturas.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> ahuyama, caldo vegetal, mantequilla, hogao.<br>
+                                </p> 
+                            </div>
+                        </div>           
+                    </div>
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/sancocho-Trifasico.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class=texto>
+                                <h2><b>Sancocho Trifásico</b></h2>
+                                <hr>
+                                <p>Se llama así porque todos los ingredientes van picaditos.<br>
+                                A diferencia del tradicional, en el que se cocinan carnes enteras<br> 
+                                con trozos grandes de papa, plátano y yuca.<br>
+                                </p> 
+                                <p><strong>Ingredientes:</strong> plátanos verdes, yuca, papas, caldo de carne o de gallina, carne, zanahoria, 
+                                apio, ahuyama, maiz.<br>
+                                </p> 
+                            </div>
+                        </div>                       
+                    </div>            
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/sobrebarriga-Horno.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Sobrebarriga</b></h2>
+                                <hr>
+                                <p>La sobrebarriga al horno es uno de mis platos colombianos favoritos. <br>
+                                Es tan fácil que solo requiere marinarla con anticipación<br>
+                                y luego dejarla cocer en una olla a presión.<br>
+                                </p>
+                                <p><strong>Ingredientes:</strong> sobrebarriga, cerveza, miga de pan.<br>
+                                </p> 
+                            </div> 
+                        </div>           
+                    </div>
+                    <div class="column">
+                        <div class="imagen_plato">
+                            <img src="../assets/sopa-Arroz.jpg" class="d-block w-100" alt="...">
+                        </div>                              
+                        <div class="overlay">
+                            <div class="texto">
+                                <h2><b>Sopa Arroz</b></h2>
+                                <hr>
+                                <p>En el momento de servirla, se añade un poco de cilantro picado<br>
+                                para realzar aun más, el delicioso sabor de la sopa.<br>
+                                </p> 
+                                <p><strong>Ingredientes: </strong> callos, arroz, zanahoria, alverjas, papa.<br>
+                                </p>
+                            </div>   
+                        </div>           
+                    </div>            
+                </div>
+            </div>                                  
+    </div>
+</template>
+
+<style scoped>
+.imagen_plato{
+    width: 70%;
+}
+#carta{
+    margin: 1px;
+    width: 100%;
+    background-color:rgb(241, 224, 201);    
+}
+#carta ul{
+    list-style: none;
+    flex:content;
+}
+ul li{
+    margin: 1%;
+    justify-content: space-around;    
+    position: relative;
+}
+#carta .column{
+    display: flex;
+    flex-flow: row wrap; 
+    flex-direction: row;
+    grid-template-columns: 1fr 2fr;   
+    align-self: center;    
+}
+#carta > h2{
+    font-size: 100px;
+    text-align: center;
+}
+#carta .column h2{
+    font-size: 50px;
+    
+}
+#carta > h4{
+    font-size: 40px;
+    text-align: center;
+    margin-bottom: 3%;
+}
+.imagen_plato img{
+    width: 600px;
+    height:400px;
+    border-radius: 20%;
+    float: left;
+    border-style: solid; 
+    border-width: 3px;
+    border-color:black; 
+}
+
+.column{
+    margin-bottom: 3%;
+    position: relative;
+    width: 50%;
+    float: left;
+    justify-content: center;
+}
+#carta .column >img{
+    margin-right: 1px;
+}
+#carta .column > p{   
+    width: 1500px;
+    text-align: justify;
+    margin: 10px;    
+    padding-left: 20px;
+    font-size: 25px;
+    font-family:Arial, Helvetica, sans-serif;
+    display:flexbox;  
+    vertical-align:baseline; 
+}
+.imagen {
+    display: block;
+    width: 100%;
+    height: auto;
+}
+.overlay {
+    margin-left: 15%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right:0;
+    width: 600px;
+    height:auto;
+    border-radius: 20%;
+    opacity: 0;
+    transition: .5s ease;
+    background-color:khaki;
+}  
+.column:hover .overlay {
+    opacity: 1;    
+}  
+.texto {
+    width: 90%;
+    margin: 1px 1px;    
+    border-radius: 10%;
+    padding: 1%;
+    color:brown;
+    font-size: 2vh;
+    position:absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    text-align:center;
+}
+</style>
---
+++ restaurante_hcbb_web/src/components/MenuSemanal.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="MenuSemanal">
+        <h1>Menú de la semana</h1><br>
+        <h3><em>¡Escoge el día y descubre el menú!</em></h3><br> 
+        <div class="semana">
+            <div class="lunes">
+                <a target=_self href="menu_Lunes.html">
+                    <img id="Lunes" src="src/assests/lunes.png" alt="Lunes">
+                </a>                                           
+            </div>    
+            <div class="martes">
+                <a target=_self href="menu_Martes.html">
+                    <img id="Martes" src="src/assets/martes.png" alt="Martes">
+                </a>                        
+            </div>                                  
+            <div class="miercoles">
+                <a target=_self href="menu_Miercoles.html">
+                    <img id="Miercoles" src="src/assets/miercoles.png" alt="Miércoles">
+                </a>                        
+            </div>      
+            <div class="jueves">
+                <a target=_self href="menu_Jueves.html">
+                    <img id="Jueves" src="src/assets/jueves.png" alt="Jueves">
+                </a>                        
+            </div> 
+            <div class="viernes">
+                <a target=_self href="menu_Viernes.html">
+                    <img id="Viernes" src="/src/assets/viernes.png" alt="Viernes">
+                </a>                        
+            </div>                      
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#MenuSemanal img{
+    width: 300px;;
+    height: 300px;;
+    border-radius: 50%;
+}
+.semana {
+    display:flex;   
+    justify-content: space-around;
+    margin: 0%;
+}
+
+#MenuSemanal{
+    padding: 1%;
+    margin: 0.5%;
+    width: 100%;
+    border-style: solid; 
+    border-width: 3px;
+    border-color:rgb(226, 27, 27);     
+}
+#MenuSemanal > h1{
+    font-size: 50px;
+}
+#MenuSemanal > h3{
+    font-size: 30px;
+}
+#Lunes:hover{
+    transform: scale(150%);
+    transition: .5s ease;
+}
+#Martes:hover{
+    transform: scale(150%);
+    transition: .5s ease;
+}
+#Miercoles:hover{
+    transform: scale(150%);
+    transition: .5s ease;
+}
+#Jueves:hover{
+    transform: scale(150%);
+    transition: .5s ease;
+}
+#Viernes:hover{
+    transform: scale(150%);
+    transition: .5s ease;
+}
+</style>
